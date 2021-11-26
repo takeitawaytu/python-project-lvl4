287474f583db2564a08e9ec655a0300dba7dac00
takeitawaytu <takeitawaytu@gmail.com>
2021-11-26 18:44:01 +0300

    fix wsgi

--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'templates.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
 
 application = get_wsgi_application()
 
